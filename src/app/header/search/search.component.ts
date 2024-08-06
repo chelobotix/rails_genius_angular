3e00eb135acb50764c562bd2a0be358c17da9741
Marcelo <marceloalarconbarrenechea@gmail.com>
2024-08-06 15:13:59 -0400

    feature(search): add search modal

--- src/app/header/search/search.component.ts
+++ src/app/header/search/search.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core'
 import { Button } from 'primeng/button'
+import { DialogModule } from 'primeng/dialog'
+import { FormsModule } from '@angular/forms'
+import { posts } from './posts'
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [Button],
+  imports: [Button, DialogModule, FormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
-export class SearchComponent {}
+export class SearchComponent {
+  visible: boolean = false
+  searchData: string = ''
+
+  showDialog() {
+    this.visible = true
+  }
+
+  handleSearch() {
+    console.log('s')
+  }
+
+  protected readonly posts = posts
+}
