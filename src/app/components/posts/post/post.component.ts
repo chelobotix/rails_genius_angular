5b0d749c81b86d4cdf31890e0c8ac341b8462ffd
Marcelo <marceloalarconbarrenechea@gmail.com>
2024-09-25 08:53:55 -0400

    feature(tinymce): add Post part III

--- src/app/components/posts/post/post.component.ts
+++ src/app/components/posts/post/post.component.ts
@@ -2,9 +2,11 @@ import { Component, inject, OnInit, signal } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { PostService } from '../../../services/post.service'
 import { IPost } from '../../../models/post.model'
-import { catchError, filter, of, switchMap } from 'rxjs'
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser'
 import { LoaderService } from '../../../services/loader.service'
 import { AvatarModule } from 'primeng/avatar'
+
+declare var lightbox: any
 
 @Component({
   selector: 'app-post',
@@ -17,18 +19,25 @@ export class PostComponent implements OnInit {
   private route = inject(ActivatedRoute)
   private postService = inject(PostService)
   private loaderService = inject(LoaderService)
+  private sanitizer = inject(DomSanitizer)
+  public safeHtmlBody: SafeHtml = ''
 
   postId: string | null = null
   post = signal<IPost | null>(null)
   loader = this.loaderService.loadingState
 
   ngOnInit() {
+    lightbox.option({
+      resizeDuration: 200,
+      wrapAround: true,
+    })
     this.loaderService.showLoader()
     this.postId = this.route.snapshot.paramMap.get('id')
     if (this.postId) {
       this.postService.getPost(this.postId).subscribe({
         next: (response) => {
           this.post.set(response.post)
+          this.safeHtmlBody = this.sanitizer.bypassSecurityTrustHtml(this.post()?.body as string)
           this.loaderService.hideLoader()
         },
         error: (error) => {
