fd20d54e0588e686327648e7e31d90d660754fb5
Marcelo <marceloalarconbarrenechea@gmail.com>
2024-10-23 09:25:10 -0400

    feature(comments): Add link to Hero posts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,21 +1,29 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core'
-import { MenuService } from '../../services/menu.service'
+import { Component, inject, OnInit } from '@angular/core'
 import { NgClass } from '@angular/common'
 import { HeroComponent } from './hero/hero.component'
 import { PostService } from '../../services/post.service'
 import { LoaderService } from '../../services/loader.service'
-import { delay, Subscription, tap } from 'rxjs'
-import { IPosts } from '../../models/posts.model'
+import { Subscription, tap } from 'rxjs'
 import { FeatureComponent } from './feature/feature.component'
 import { DividerModule } from 'primeng/divider'
 import { PostHolderComponent } from './post-holder/post-holder.component'
 import { PostComponent } from '../posts/post/post.component'
 import { IPost } from '../../models/post.model'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgClass, HeroComponent, FeatureComponent, PostComponent, DividerModule, PostHolderComponent, PostComponent],
+  imports: [
+    NgClass,
+    HeroComponent,
+    FeatureComponent,
+    PostComponent,
+    DividerModule,
+    PostHolderComponent,
+    PostComponent,
+    RouterLink,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
